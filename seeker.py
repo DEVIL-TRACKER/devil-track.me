d446906898da28f45068dea905aba658d2e6644f
DEVIL-TRACKER <148515609+DEVIL-TRACKER@users.noreply.github.com>
2023-10-20 11:45:44 +0530

    Update seeker.py

--- seeker.py
+++ seeker.py
@@ -114,7 +114,7 @@ def banner():
 		comms_url = json_data['comms']
 
 	art = r'''
-logo = f"""
+logo = f'''
 {blue}______  ___  ______ _   __    __   __ _____ 
 {blue}|  _  \/ _ \ | ___ \ | / /    \ \ / /|  _  |
 {blue}| | | / /_\ \| |_/ / |/ /______\ V /  \ V / 
@@ -122,7 +122,7 @@ logo = f"""
 {blue}| |/ /| | | || |\ \| |\  \    / /^\ \| |_| |
 {blue}|___/ \_| |_/\_| \_\_| \_/    \/   \/\_____/
 {red}{" "*35}         [{blue}v{version[:17]}{yellow}]
-{cyan}{" "*36}[{blue}By {red}Error.2005.03{cyan}]"""
+{cyan}{" "*36}[{blue}By {red}Error.2005.03{cyan}]'''
 
 
 def send_webhook(content, msg_type):
